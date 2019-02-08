7a5ef286eb5cea7cf9daf6e082c8cd32d06d7a11
Antonio <antonio241710@gmail.com>
2019-02-08 23:59:45 +0100

    added migration Ship

---
+++ database/migrations/2019_02_08_223407_create_ships_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ships', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('x');
+            $table->integer('y');
+            $table->string('axis');
+            $table->integer('length');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ships');
+    }
+}
