120d10af2a5d6405fbb2c673f4bfbb4de315d5d2
Antonio <antonio241710@gmail.com>
2019-02-10 23:51:14 +0100

    added ship_id foreign key

--- database/migrations/2019_02_08_230240_create_shots_table.php
+++ database/migrations/2019_02_08_230240_create_shots_table.php
@@ -17,10 +17,12 @@ class CreateShotsTable extends Migration
             $table->increments('id');
             $table->integer('x');
             $table->integer('y');
-            $table->unsignedInteger('user_id')->nullable();
+            $table->unsignedInteger('user_id')();
+            $table->unsignedInteger('ship_id')->nullable();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('ship_id')->references('id')->on('ships');
         });
     }
 
