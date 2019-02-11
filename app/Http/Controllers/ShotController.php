3bf8b07b5fc1940071a891e8e7eb726eece3f91b
Antonio <antonio241710@gmail.com>
2019-02-11 23:39:53 +0100

    store shot and update ship if success

--- app/Http/Controllers/ShotController.php
+++ app/Http/Controllers/ShotController.php
@@ -36,10 +36,76 @@ class ShotController extends Controller
      */
     public function store(ShotRequest $shotRequest)
     {
-        var_dump($shotRequest->data['col']);
-        var_dump($shotRequest->data['row']);
-        die('done!');
+        $shots = auth()->user()->shots()->get();
+        $shipFired = null;
+        $state = 0; //water or success
 
+        foreach ($shots as $shot) {
+            if ($shot->x == $shotRequest->data['x'] && $shot->y == $shotRequest->data['y'] ){
+
+                $shot = Shot::create([
+                    'x' => $shot->x,
+                    'y' => $shot->y,
+                    'ship_id' => $shot->ship_id,
+                    'user_id' => $shot->user_id,
+                ]);
+
+                return response()->json([
+                    'state' => $state,
+                    'ship' => $shipFired,
+                ]);
+            }
+        }
+
+        $shipId = null;
+        $ships = auth()->user()->ships()->get();
+
+        foreach ($ships as $ship){
+
+            $x = $ship->x;
+            $y = $ship->y;
+
+            for ($i = 0; $i < $ship->length; $i++){
+
+                if ($x == $shotRequest->data['x'] && $y == $shotRequest->data['y'] ){
+                    $state = 1;
+                    $shipId = $ship->id;
+                    $shot_counter = $ship->shot_counter + 1;
+
+                    $ship->update([
+                        'shot_counter' => $shot_counter,
+                    ]);
+
+                    $shipFired = $ship;
+
+                    break;
+                }
+
+                if ($ship->axis === 'H'){
+                    $y++;
+                }
+                else{
+                    $x++;
+                }
+            }
+
+            if (!is_null($shipFired)){
+                break;
+            }
+        }
+
+        Shot::create([
+            'x' => $shotRequest->data['x'],
+            'y' => $shotRequest->data['y'],
+            'user_id' => auth()->user()->id,
+            'ship_id' => $shipId,
+        ]);
+
+
+        return response()->json([
+            'state' => $state,
+            'ship' => $shipFired,
+        ]);
     }
 
     /**
