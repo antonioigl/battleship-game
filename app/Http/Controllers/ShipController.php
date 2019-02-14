1336ae98ca96f408756b30d9d101a5aee899e634
Antonio <antonio241710@gmail.com>
2019-02-14 23:47:37 +0100

    added function ships in grid

--- app/Http/Controllers/ShipController.php
+++ app/Http/Controllers/ShipController.php
@@ -24,7 +24,101 @@ class ShipController extends Controller
      */
     public function create()
     {
+//        $shipsCount = auth()->user()->ships()->count();
+//
+//        if ($shipsCount){
+//            return view('ships.create');
+//        }
+
+        $userId = auth()->user()->id;
+//        Ship::where('user_id', $userId)->delete();
+        $ships = ['carrier' => 5, 'battleship' => 4, 'submarine' => 3, 'destroyer' => 2];
+
+        foreach ($ships as $ship){
+
+            $isValidShipInGrid = false;
+
+            while (!$isValidShipInGrid){
+
+                $x = rand(1,10);
+                $y = rand(1,10);
+                $size = $ship;
+                $axis = ['V', 'H'];
+
+                $key = array_rand($axis);
+                $axis = $axis[$key];
+
+                $isValidShipInGrid = $this->validateShipInGrid($x, $y, $size, $axis);
+            }
+
+            var_dump($x);
+            var_dump($y);
+            var_dump($axis);
+            var_dump($ship);
+            var_dump($userId);
+//            dd('done');
+
+            $sucess = Ship::create([
+                'x' => $x,
+                'y' => $y,
+                'axis' => $axis,
+                'length' => $ship,
+                'shot_counter' => 0,
+                'user_id' => $userId,
+            ]);
+
+
+        }
+
+        die('done!');
+
         return view('ships.create');
+    }
+
+    private function validateShipInGrid($x, $y, $size, $axis)
+    {
+        $ships = auth()->user()->ships()->get();
+
+        if ( ($axis == 'H') && (($x + $size) > 10) ){
+            return false;
+        }
+
+        if ( ($axis == 'V') && (($y + $size) > 10) ){
+            return false;
+        }
+
+        $xCount = $x;
+        $yCount = $y;
+
+        foreach ($ships as $ship) {
+            $sx = $ship->x;
+            $sy = $ship->y;
+            for ($i = 0; $i < $ship->length; $i++){
+                for ($j = 0; $j < $size; $j++){
+                    if ($sx == $xCount && $sy == $yCount){
+                        return false;
+                    }
+                    if ($axis == 'H'){
+                        $xCount++;
+                    }
+                    else{
+                        $yCount++;
+                    }
+                }
+
+                $xCount = $x;
+                $yCount = $y;
+
+                if ($ship->axis == 'H'){
+                    $sx++;
+                }
+                else{
+                    $sy++;
+                }
+            }
+        }
+
+        return true;
     }
 
     /**
