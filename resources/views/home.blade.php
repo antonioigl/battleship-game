daf36e13f72d53c6ec7462b87727e729c8d4b2fa
Antonio <antonio241710@gmail.com>
2019-02-16 18:05:09 +0100

    added new game modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 @section('title', 'Home')
 
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -15,12 +14,18 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                        <a href="{{route('ships.create')}}" class="btn btn-secondary btn-lg btn-block">CONTINUE</a>
-                        <a href="{{route('ships.create')}}" class="btn btn-primary btn-lg btn-block">NEW GAME</a>
-                        <a href="" class="btn btn-primary btn-lg btn-block">RANKING</a>
+                    @if(auth()->user()->ships()->count())
+                        <a href="{{route('ships.play')}}" class="btn btn-secondary btn-lg btn-block">CONTINUE</a>
+                        <button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#newGameModal">NEW GAME</button>
+                    @else
+                            <a href="{{route('ships.create')}}" class="btn btn-primary btn-lg btn-block">NEW GAME</a>
+                    @endif
+                    <a href="" class="btn btn-primary btn-lg btn-block">RANKING</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@include('modals.new-game')
 @endsection
+
