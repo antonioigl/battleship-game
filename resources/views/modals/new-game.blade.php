38d0750f9685268e5fb709ef173471343458ff16
Antonio <antonio241710@gmail.com>
2019-02-17 18:53:34 +0100

    added new game modal

--- resources/views/modals/new-game.blade.php
+++ resources/views/modals/new-game.blade.php
@@ -1,8 +1,8 @@
-<div class="modal fade" id="newGameModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="newGameModal" tabindex="-1" role="dialog" aria-labelledby="newGameModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">NEW GAME</h5>
+                <h5 class="modal-title" id="newGameModalLabel">NEW GAME</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
