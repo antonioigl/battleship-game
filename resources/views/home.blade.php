d483109ac455d2e0fd4a5385ae7a7ee0eec605f7
Antonio <antonio241710@gmail.com>
2019-02-18 00:10:46 +0100

    added scores links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,13 +14,16 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    @if(auth()->user()->ships()->count())
+                    @if(App\Ship::isGameOver())
+                        <a href="{{route('ships.create')}}" class="btn btn-primary btn-lg btn-block">NEW GAME</a>
+                    @elseif( auth()->user()->ships()->count() )
                         <a href="{{route('ships.play')}}" class="btn btn-secondary btn-lg btn-block">CONTINUE</a>
                         <button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#newGameModal">NEW GAME</button>
                     @else
-                            <a href="{{route('ships.create')}}" class="btn btn-primary btn-lg btn-block">NEW GAME</a>
+                        <a href="{{route('ships.create')}}" class="btn btn-primary btn-lg btn-block">NEW GAME</a>
                     @endif
-                    <a href="" class="btn btn-primary btn-lg btn-block">RANKING</a>
+                    <a href="{{route('scores.show')}}" class="btn btn-primary btn-lg btn-block">MY SCORES</a>
+                    <a href="{{route('scores.index')}}" class="btn btn-primary btn-lg btn-block">RANKING</a>
                 </div>
             </div>
         </div>
