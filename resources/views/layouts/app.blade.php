84287ae28ff2e1f380626029c1f6276588f67ad2
Antonio <antonio241710@gmail.com>
2019-02-18 00:13:34 +0100

    deleted link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,9 +59,6 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}">
-                                        {{ __('My Scores') }}
-                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
