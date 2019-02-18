89a7e3dd06872b9fa3d38aa97827a8ca5ef03661
Antonio <antonio241710@gmail.com>
2019-02-18 23:05:03 +0100

    removed comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,8 +82,6 @@
     </div>
 </body>
 
-{{--<script src="{{asset('js/jquery.min.js')}}"></script>--}}
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 @yield('script')
